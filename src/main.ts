495add47a8f548065d22cc995b504fc606ab32a0
LautaroAlfonso <sebastiansosa3011@gmail.com>
2025-11-03 12:57:26 -0300

    cambios

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import {DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: '', // Permitir todas las solicitudes CORS
+  })
+
   const config = new DocumentBuilder()
     .setTitle('TRAIN-X')
     .setDescription('Documentación de API para TRAIN-X')
