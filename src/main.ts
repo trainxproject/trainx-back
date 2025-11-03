8e453a453651b3e863891a666efc4904d768a124
lautaroalfonso423 <sebastiansosa3011@gmail.com>
2025-11-03 14:14:20 -0300

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import {DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import ngrok from "ngrok"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,12 +12,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, 
   })
-
-
-  ngrok.authtoken(process.env.NGROK_AUTHTOKEN as string)
-  .then(()=> ngrok.connect(3000))
-  .then(url => console.log('🔄 Webhook URL para Mercado Pago:', url + '/webhooks/mercadopago'))
-  .catch(err => console.error('❌ Error ngrok'))
   
   const config = new DocumentBuilder()
     .setTitle('TRAIN-X')
